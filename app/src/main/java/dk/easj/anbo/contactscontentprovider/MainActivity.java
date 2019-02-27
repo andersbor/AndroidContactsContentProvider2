6e320e5a66e27f98e53682427100aba3034ee885
Anders Børjesson <anbo@zealand.dk>
2019-02-27 10:54:43 +0100

    permissions

--- app/src/main/java/dk/easj/anbo/contactscontentprovider/MainActivity.java
+++ app/src/main/java/dk/easj/anbo/contactscontentprovider/MainActivity.java
@@ -1,27 +1,33 @@
 package dk.easj.anbo.contactscontentprovider;
 
+import android.Manifest;
 import android.app.ListActivity;
 import android.content.CursorLoader;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.ContactsContract;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends ListActivity {
+public class MainActivity extends AppCompatActivity {
+    public static final String LOG_TAG = "contactsProvider";
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-
-        ListView listView = getListView();
+        setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.mainContactsListView);
         TextView header = new TextView(this);
         // http://stackoverflow.com/questions/9494037/how-to-set-text-size-of-textview-dynamically-for-different-screens
         header.setTextSize(header.getTextSize() * 1.1f);
@@ -32,9 +38,19 @@ public class MainActivity extends ListActivity {
     @Override
     protected void onStart() {
         super.onStart();
+
         // Android 6 requires permission in manifest + at runtime
         // https://developer.android.com/training/permissions/requesting.html
         // http://stackoverflow.com/questions/35572694/android-6-0-marshmallow-read-contacts-permission-allows-to-read-contacts-name
+        int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS);
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            String message = "Missing permission: contacts";
+            Log.d(LOG_TAG, message);
+            // Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            TextView messageView = findViewById(R.id.mainMessageTextView);
+            messageView.setText(message);
+            return;
+        }
 
         //Uri allContacts = Uri.parse("content://contacts/people");
         Uri allContacts = ContactsContract.Contacts.CONTENT_URI;
@@ -54,13 +70,21 @@ public class MainActivity extends ListActivity {
         int[] views = {R.id.list_item_contactID, R.id.list_item_contactName};
         SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.list_item_layout, cursor,
                 columns, views, CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
-        setListAdapter(adapter);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), "Yoy clicked no. " + id, Toast.LENGTH_LONG).show();
+                Log.d("CONTACT", "You clicked no. " + id);
+            }
+        });
     }
-
+/*
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
         Toast.makeText(this, "Yoy clicked no. " + id, Toast.LENGTH_LONG).show();
         Log.d("CONTACT", "You clicked no. " + id);
     }
+    */
 }
